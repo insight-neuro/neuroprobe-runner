dcf2341061105f1c20480117b54209eb794d4836
alexander-brady <bradya@student.ethz.ch>
2026-05-03 22:16:12 -0400

    feat: subject helpers

--- neuroprobe/subject.py
+++ neuroprobe/subject.py
@@ -30,9 +30,30 @@ class BrainTreebankSubject:
             keep_files_open=keep_files_open,
         )
 
-    def load_neural_data(self, trial_id: int, start: float, end: float) -> CraneFeature:
+    @property
+    def trials(self) -> list[int]:
+        """Return a list of trial IDs for this subject."""
+        return [
+            int(rec_id.split("_ses-")[1].split("_")[0])
+            for rec_id in self.dataset.recording_ids
+        ]
+
+    def trial_interval(self, trial_id: int) -> tuple[float, float]:
+        """Return the start and end times for the given trial."""
+        recording_id = f"sub-{self.subject_id:03}_ses-{trial_id:02}"
+        domain = self.dataset.get_sampling_intervals()[recording_id]
+        return domain.start.item(), domain.end.item()  # type: ignore[attr-defined]
+
+    def load_neural_data(
+            self, trial_id: int, start: float | None = None, end: float | None = None
+        ) -> CraneFeature:
         """Load neural data for the given trial and time window."""
         recording_id = f"sub-{self.subject_id:03}_ses-{trial_id:02}"
+
+        if start is None or end is None:
+            interval = self.trial_interval(trial_id)
+            start = start if start is not None else interval[0]
+            end = end if end is not None else interval[1]
 
         idx = DatasetIndex(recording_id=recording_id, start=start, end=end)
         data = self.dataset[idx]
